a513f1fd5ed07dc363f0bff5d87bce81e916a6d6
aa <1044393721@qq.com>
2016-06-04 18:31:19 +0800

    test github

--- app/src/main/java/com/example/lenovo/eventbustest/SecondActivity1.java
+++ app/src/main/java/com/example/lenovo/eventbustest/SecondActivity1.java
@@ -4,4 +4,8 @@ package com.example.lenovo.eventbustest;
  * Created by lenovo on 4/6/2016.
  */
 public class SecondActivity1 {
+    public void test()
+    {
+        System.out.println("my first github project");
+    }
 }
